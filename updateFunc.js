a8ae735351449bfa861dffdb87104f496db44330
Connor Machado <cmachado@vassar.edu>
2023-04-26 20:11:40 -0400

    Buttons sorta work, database is next

---
+++ updateFunc.js
@@ -0,0 +1,9 @@
+function updateFunc(winner){
+    srcy = document.getElementById(winner);
+    
+    console.log(srcy)
+
+    // var element = document.getElementById('txt');
+    // var text = element.innerText || element.textContent;
+    // element.innerHTML = text;
+}
