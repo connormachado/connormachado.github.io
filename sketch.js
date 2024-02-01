3745a60c5bfa8bdad92a037265e9b66886a54bca
Connor Machado <cmachado@vassar.edu>
2024-02-01 10:32:44 -0500

    Fixed script bug.
    The drawing screen and code now works. Dumb error took me hours to solve. So dumb, so programmer.

--- sketch.js
+++ sketch.js
@@ -2,17 +2,36 @@ function setup() {
     createCanvas(720, 400);
     background(230);
     strokeWeight(2);
-    ellipse(50,50,80,80);
+    ellipseList = [];
+  }
+
+  function keepEllipse(x, y, a, b, color){
+    //Save an ellipse for later use
+    newE = [x, y, a, b, color]
+    ellipseList.push(newE)
+    console.log(newE, ellipseList.length)
   }
   
   function draw() {
     background(220);
-    
-    if (mouseIsPressed) {
-      stroke(255);
-    } else {
-      stroke(237, 34, 93);
+
+    //Draw previously clicked ellipses from earlier
+    for (let i=0; i < ellipseList.length; i++) {
+      tempE = ellipseList[i];
+      ellipse(tempE[0], tempE[1], tempE[2], tempE[3])
+      fill(tempE[4])
     }
+ 
+    //The aim of the function, change this later
     line(mouseX - 66, mouseY, mouseX + 66, mouseY);
     line(mouseX, mouseY - 66, mouseX, mouseY + 66);
+  }
+
+  function mouseClicked(){
+    //Mouse clicks once
+    //Add in the radius changer here
+    // https://editor.p5js.org/awade5/sketches/H1W0g19BX
+    myColor = color(random(255), random(255), random(255));
+    keepEllipse(mouseX, mouseY, 20, 20, myColor)
+    stroke(255);
   }
