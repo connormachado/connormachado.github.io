43b4968e3a611f5a7008ab744f5750af0ad7f4ff
Connor Machado <cmachado@vassar.edu>
2024-02-07 07:57:32 -0500

    Updated makeImage.py
    I can now make a white picture with the dimensions of the picture of my choosing.

--- makeImage.py
+++ makeImage.py
@@ -1,13 +1,26 @@
 from PIL import Image
 import numpy as np
 
-def random_img(output, width, height):
 
-    array = np.random.randint(0,255, (height,width,3))  
+# pixels = [
+#    [(54, 54, 54), (232, 23, 93), (71, 71, 71), (168, 167, 167)],
+#    [(204, 82, 122), (54, 54, 54), (168, 167, 167), (232, 23, 93)],
+#    [(71, 71, 71), (168, 167, 167), (54, 54, 54), (204, 82, 122)],
+#    [(168, 167, 167), (204, 82, 122), (232, 23, 93), (54, 54, 54)]
+# ]
 
-    array = np.array(array, dtype=np.uint8)
-    img = Image.fromarray(array)
-    img.save(output)
+# pixels = []
+# for y in range(2280):
+#     for x in range(1284):
+#         pixels.append( (255,255,255) )
 
 
-random_img('random.png', 100, 50)
+img = np.ones(( 2280, 1284, 3), dtype = np.uint8)
+img = 255 * img
+
+# Convert the pixels into an array using numpy
+array = np.array(img, dtype=np.uint8)
+
+# Use PIL to create an image from the new array of pixels
+new_image = Image.fromarray(array)
+new_image.save('test2.png')
